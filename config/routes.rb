fd67dfa5c84a5f1f3e4277c77c5e7f2ba11df076
Vinicius Pontes <vinilodeon@hotmail.com>
2018-12-05 15:22:51 +0100

    AB04 Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/show'
+  #get 'user/show'
+  get '/user/:id', to: 'user#show', as: 'user'
+
   root 'pages#home'
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
+
 end
